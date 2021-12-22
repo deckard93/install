aef0f531d686f720f537123de14568a305e2ae19
Teodor Petrescu <p.teo93@gmail.com>
2021-12-22 17:08:16 +0000

    remove dead code and add comments

--- setup_install_job.ps1
+++ setup_install_job.ps1
@@ -1,8 +1,11 @@
+# allow running scripts
 Set-ExecutionPolicy unrestricted -Force
 
+# download install script
 New-Item -ItemType Directory -Force -Path C:\scripts
 (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/deckard93/install/master/win_install.ps1", "C:\scripts\startup.ps1")
 
+# autologin for restarts
 $Username = Read-Host 'Enter username for auto-logon'
 $Pass = Read-Host "Enter password for $Username"
 $RegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
@@ -10,10 +13,10 @@ Set-ItemProperty $RegistryPath 'AutoAdminLogon' -Value "1" -Type String
 Set-ItemProperty $RegistryPath 'DefaultUsername' -Value "$Username" -type String 
 Set-ItemProperty $RegistryPath 'DefaultPassword' -Value "$Pass" -type String
 
+# schedule install job to run
 $Trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
-$Action=New-ScheduledTaskAction -Execute PowerShell.exe -WorkingDirectory C:/Scripts -Argument  "-command C:\scripts\startup.ps1 *> C:\scripts\log.txt" 
-#$Principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
-#Register-ScheduledTask -TaskName "Install" -Trigger $Trigger -Action $Action -Principal $Principal
+$Action=New-ScheduledTaskAction -Execute PowerShell.exe -WorkingDirectory C:/Scripts -Argument  "-command C:\scripts\startup.ps1" 
 Register-ScheduledTask -TaskName "Install" -Trigger $Trigger -Action $Action  -RunLevel Highest
 
+# restart to start install
 shutdown /r
